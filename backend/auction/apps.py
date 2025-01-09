3b06755e503dbdb1c718f7391e1f779c233c3a8e
lmProgramming <mikikubs@gmail.com>
2025-01-09 11:43:57 +0100

    feat: mqtt now runs only on manage.py runserver

--- backend/auction/apps.py
+++ backend/auction/apps.py
@@ -1,10 +1,15 @@
 from django.apps import AppConfig
+import sys
 
 
 class AuctionConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'auction'
+    
+    def ready(self, *args, **kwargs):
+        is_manage_py = any(arg.casefold().endswith("manage.py") for arg in sys.argv)
+        is_runserver = any(arg.casefold() == "runserver" for arg in sys.argv)
 
-    def ready(self):
-        from .mqtt_client import start_mqtt
-        start_mqtt()
+        if is_manage_py and is_runserver:
+            from .mqtt_client import start_mqtt
+            start_mqtt()
