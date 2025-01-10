d4dd9baa7fa7e11b3d97137af57582027399b328
Patryk Łuszczek <52531090+Wasloso@users.noreply.github.com>
2025-01-10 19:25:39 +0100

    refractored client a bit to allow connection and bidding on windows (bidding not implemented yet)

---
+++ raspberry_pi/client/platform/windows.py
@@ -0,0 +1,38 @@
+import json
+from client.clientdevice import ClientDevice
+import keyboard
+
+from connection_config import TOPIC_SUBSCRIBE  # Install with `pip install keyboard`
+
+
+class Windows(ClientDevice):
+    def displayAuction(self, auction, status):
+        print(f"Windows: {auction} - {status}")
+
+    def on_connect(self, client, userdata, flags, rc, properties=None):
+        print(f"Windows: on_connect - {rc}")
+        super().on_connect(client, userdata, flags, rc, properties)
+
+    def on_message(self, client, userdata, message):
+        topic, payload = self.decode(message)
+        if topic == TOPIC_SUBSCRIBE:
+            auction = json.loads(payload)
+            print(
+                f"Received auction data: ID: {auction['auction_id']}, "
+                f"Article: {auction['article']}, "
+                f"Price: {auction['current_price']}"
+            )
+
+    def on_publish(self, client, userdata, mid):
+        print(f"Windows: on_publish - {mid}")
+
+    def read_card(self):
+        event = keyboard.read_event()
+        if key := event.event_type == keyboard.KEY_DOWN:
+            key = event.name
+            if key.isdigit():
+                return (int(key), 1)
+        return (None, 0)
+
+    def clear(self):
+        pass
