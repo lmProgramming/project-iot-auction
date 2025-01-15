2613e6ff4b41304b8d19a29c81d6e0c5cb16881f
Patryk Łuszczek <52531090+Wasloso@users.noreply.github.com>
2025-01-15 17:48:54 +0100

    allow registering with card id

--- backend/auction/views.py
+++ backend/auction/views.py
@@ -55,54 +55,73 @@ class WalletListView(ListView):
 
 class AuctionCreateView(CreateView):
     model = Auction
-    fields = ['article', 'start_time', 'end_time', 'current_price']
+    fields = ["article", "start_time", "end_time", "current_price"]
     template_name = "auction_form.html"
-    success_url = reverse_lazy('auction_list')
+    success_url = reverse_lazy("auction_list")
 
 
 class ArticleCreateView(CreateView):
     model = Article
-    fields = ['name', 'owner', 'starting_price', 'description', 'image']
+    fields = ["name", "owner", "starting_price", "description", "image"]
     template_name = "article_form.html"
-    success_url = reverse_lazy('article_list')
+    success_url = reverse_lazy("article_list")
 
 
 class UserCreateView(CreateView):
     model = User
-    fields = ['name', 'surname', 'login', 'password', 'age', 'wallet']
+    fields = ["name", "surname", "login", "password", "age", "wallet"]
     template_name = "user_form.html"
-    success_url = reverse_lazy('user_list')
+    success_url = reverse_lazy("user_list")
 
 
 class WalletCreateView(CreateView):
     model = Wallet
-    fields = ['card_id', 'balance']
+    fields = ["card_id", "balance"]
     template_name = "wallet_form.html"
-    success_url = reverse_lazy('wallet_list')
+    success_url = reverse_lazy("wallet_list")
 
 
 def register(request, card_id: int):
-    try:
-        wallet_qs = Wallet.objects.filter(card_id=card_id)
-        if wallet_qs.exists():
-            return HttpResponse("User already registered")
-    except Wallet.DoesNotExist:
-        wallet = Wallet.objects.create(card_id=card_id, balance=0.0)
+    wallet = None
+    wallet_qs = Wallet.objects.filter(card_id=card_id)
+
+    # Create wallet if it doesn't exist
+    if wallet_qs.exists():
+        wallet = wallet_qs.first()
+        return redirect("/")
 
     if request.method == "POST":
         form = RegistrationForm(request.POST)
         if form.is_valid():
+            wallet = Wallet.objects.create(card_id=card_id, balance=2500)
             user = User.objects.create(
                 name=form.cleaned_data["name"],
                 surname=form.cleaned_data["surname"],
                 age=form.cleaned_data["age"],
+                login=form.cleaned_data["login"],
+                password=form.cleaned_data["password"],
                 wallet=wallet,
             )
-
+            print(user)
             return redirect("/")
+        else:
+            print(form.errors)
     else:
         form = RegistrationForm()
+
     return render(request, "register.html", {"form": form, "card_id": card_id})
+
+
+def check_registered(request, card_id: int):
+    try:
+        wallet_qs = Wallet.objects.filter(card_id=card_id)
+        if wallet_qs.exists():
+            user = User.objects.get(wallet=wallet_qs.first())
+            return JsonResponse({"registered": True, "user": user.name})
+        return JsonResponse({"registered": False, "url": f"/register/{card_id}/"})
+
+    except Wallet.DoesNotExist:
+        return JsonResponse({"registered": False, "url": f"/register/{card_id}/"})
 
 
 # Create your views here.
